5c165e48a9669330424b8d89cfdd66fbf4a188a6
Mateusz <cieslik@int.pl>
2024-01-25 11:29:45 +0100

    testcommit2

---
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'Simply a test', 'https://hips.hearstapps.com/hmg-prod/images/delish-202002-pozole-0392-landscape-pf-1582315071.jpg?crop=1xw:0.8441943127962085xh;center,top&resize=1200:*')
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+}
